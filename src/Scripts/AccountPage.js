4e0bd260eec688f6e07e1150b26c0ae9625622f7
JacobWingstrom <jacobwingstrom@arizona.edu>
2026-03-24 10:26:17 -0700

    Adding in base template for buttons and content

--- src/Scripts/AccountPage.js
+++ src/Scripts/AccountPage.js
@@ -2,6 +2,7 @@ import '../Sheets/AccountPage.css';
 import Header from './Header.js'
 //import pineappleLogo from '../Images/pineappleLogoPlaceholder.jpg'
 import { useAuth } from '../Context/AuthContext.js'
+import { useState } from 'react'
 
 function AccountHeader() {
     const { user } = useAuth();
@@ -13,25 +14,57 @@ function AccountHeader() {
     )
 }
 
-function Buttons() {
-    return (
+
+
+function Buttons({ setter }) {
+    return ( 
         <div className="AccountPage-Buttons">
+            <button onClick={ () => setter('MyItems') } className="AccountPage-Button" id="AccountPage-MyItems">My Items</button>
+            <button onClick={ () => setter('BidsWon') } className="AccountPage-Button" id="AccountPage-BidsWon">Bids Won</button>
+            <button onClick={ () => setter('BiddingOn') } className="AccountPage-Button" id="AccountPage-BiddingOn">Bidding On</button>
+        </div>
+    )
+}
+
+function MyItems() {
+    return (
+        <div className='AccountPage-MyItems'>
 
         </div>
     )
 }
 
-function Content() {
-    
+function BidsWon() {
+        <div className='AccountPage-BidsWon'>
+            
+        </div>
+}
+
+function BiddingOn() {
+        <div className='AccountPage-BiddingOn'>
+            
+        </div>
+}
+
+function Content({ currentPage }) {
+    return (
+        <div className="AccountPage-Content">
+            {currentPage === 'MyItems' && <MyItems />}
+            {currentPage === 'BidsWon' && <BidsWon />}
+            {currentPage === 'BiddingOn' && <BiddingOn />}
+        </div>
+    )
 }
 
 
 
 function AccountBody() {
+    const [currentPage, setCurrentPage] = useState('MyItems');
+
     return (
         <div id="AccountPage-Body">
-            <Buttons />
-            <Content />
+            <Buttons setter={ setCurrentPage }/>
+            <Content currentPage={ currentPage }/>
         </div>
     )
 }
